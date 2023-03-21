c933050a98e60ba8cd6ebe1b6a5efa1e8543ef71
Piotr Adamowicz <pad@novicell.co.uk>
2023-03-21 09:52:48 +0100

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+        <h1 class="text-9xl">
+          {{ msg }}
+        </h1>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script >
+  export default {
+    data(){
+      return{
+        msg:"Aloha!"
+      }
+    }}
+  
+</script>
